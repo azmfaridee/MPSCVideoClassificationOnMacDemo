ada4f239b1218b0b8139cc7f16da7ff84c4c5034
Abu-Zaher Faridee <faridee1@umbc.edu>
2024-02-06 12:47:11 -0500

    created a context manager to time any code block

--- webapp.py
+++ webapp.py
@@ -27,32 +27,41 @@ def home():
     return render_template('index.html', prediction=None)
 
 
+class Timer:
+    def __init__(self, print_statement="Time taken: {:.2f} seconds"):
+        self.print_statement = print_statement
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.interval = self.end - self.start
+        print(self.print_statement.format(self.interval))
+
+
 # Define route for dummy video classification
 @app.route('/classify_dummy', methods=['GET'])
 def classify_dummy_video():
     dummy_video_path = 'dummy_video.mp4'
 
     if os.path.exists(dummy_video_path):
-        # Read and preprocess video
-        video_tensor, audio_tensor, info = read_video(
-            dummy_video_path, pts_unit='sec')
+        with Timer("Time taken to preprocess the video: {:.2f} seconds") as t:
+            video_tensor, audio_tensor, info = read_video(
+                dummy_video_path, pts_unit='sec')
 
-        # shape of video_tensor: (T, H, W, C)
-        video_tensor = video_tensor.float() / 255.0
+            # shape of video_tensor: (T, H, W, C)
+            video_tensor = video_tensor.float() / 255.0
+            video_tensor = video_tensor.permute(3, 0, 1, 2)
+            video_tensor = video_tensor.unsqueeze(0)
 
-        video_tensor = video_tensor.permute(3, 0, 1, 2)
-        video_tensor = video_tensor.unsqueeze(0)
+            if use_mps:
+                video_tensor = video_tensor.to('mps')
 
-        if use_mps:
-            video_tensor = video_tensor.to('mps')
-
-        start_time = time.time()
-        with torch.no_grad():
-            outputs = model(video_tensor)
-        end_time = time.time()
-
-        print(
-            f"Time taken to classify the video: {end_time - start_time} seconds")
+        with Timer("Time taken to classify the video: {:.2f} seconds") as t:
+            with torch.no_grad():
+                outputs = model(video_tensor)
 
         predicted_class = torch.argmax(outputs, dim=1).item()
 
